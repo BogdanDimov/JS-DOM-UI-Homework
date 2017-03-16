9121d405eba55e7ca18866ea1d8d3b2c42e2b7f6
Bogdan <bogdan.dimov@outlook.com>
2017-03-16 14:27:10 +0200

    add solution for event model hw

--- Topics/07. Event Model/homework/tasks/task-1.js
+++ Topics/07. Event Model/homework/tasks/task-1.js
@@ -1,14 +1,31 @@
-/* globals $ */
-
-/* 
-
-Create a function that takes an id or DOM element and:
-  
-
-*/
-
-function solve(){
+function solve() {
   return function (selector) {
+    if (selector === null) throw Error();
+    if (typeof selector === 'undefined') throw Error();
+    if (document.getElementById(selector) === null) throw Error();
+
+    var btns = document.getElementsByClassName('button');
+    for (i = 0, len = btns.length; i < len; i += 1) {
+      btns[i].innerHTML = 'hide';
+    }
+
+    var theButton = document.getElementById('the-button');
+    if (theButton) {
+      var theContent = theButton.nextElementSibling;
+      while (theContent && theContent.className.indexOf('content') < 0) {
+        theContent = theContent.nextElementSibling;
+      }
+
+      theButton.addEventListener('click', function () {
+        if (theButton.innerHTML === 'hide') {
+          theButton.innerHTML = 'show';
+          theContent.style.display = 'none';
+        } else {
+          theButton.innerHTML = 'hide';
+          theContent.style.display = '';
+        }
+      });
+    }
   };
 };
 
